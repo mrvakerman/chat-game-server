ef8770de7e2ce76e083d338a94dcd33429356538
v.khmel <mrvakerman@gmail.com>
2019-12-12 15:13:16 +0300

    remode js files and create browserlistrc

--- src/words/words.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getWords() {
-    return require("fs").readFileSync("./src/words/rus.txt", "utf8");
-}
-exports.getWords = getWords;
