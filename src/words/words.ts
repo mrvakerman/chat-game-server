f0004dee104d1ed522f8ded114e4ea5d6a17c1a8
v.khmel <mrvakerman@gmail.com>
2019-12-12 22:11:38 +0300

    fix getting words

--- src/words/words.ts
+++ src/words/words.ts
@@ -1,4 +1,6 @@
 export function getWords(): string[] {
   const result = require("fs").readFileSync("./src/words/rus.txt", "utf8");
-  return (result as string).split("\r\n");
+  return (result as string)
+    .split("\n")
+    .map(x => x.replace("\r", "").replace("\n", ""));
 }
