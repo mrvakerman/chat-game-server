f0191394bc018b5f0dec4a4d25942f77a7ac1107
v.khmel <mrvakerman@gmail.com>
2019-12-12 16:05:09 +0300

    add public folder

---
+++ public/precache-manifest.674336c366dd1caf6355e4d40970ae0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8a5317393ec7dabea87e7c6b5f723c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd1a519e09c6cbfbf16",
+    "url": "/static/css/main.ffba4600.chunk.css"
+  },
+  {
+    "revision": "02ede641e0bc4ebc56c7",
+    "url": "/static/js/2.62abdcea.chunk.js"
+  },
+  {
+    "revision": "8bb78284ec5715f627869fe43440d08d",
+    "url": "/static/js/2.62abdcea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acd1a519e09c6cbfbf16",
+    "url": "/static/js/main.e0ea97ae.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
